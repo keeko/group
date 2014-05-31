8376d211b4265a549d8af7cbe45a39f84a65ad06
gossi <github@gos.si>
2014-05-31 18:36:14 +0200

    keeko generate:init

---
+++ src/GroupModule.php
@@ -0,0 +1,29 @@
+<?php
+namespace keeko\group;
+
+use keeko\core\module\AbstractModule;
+
+/**
+ */
+class GroupModule extends AbstractModule
+{
+    /**
+     */
+    public function install()
+    {
+    }
+
+    /**
+     */
+    public function uninstall()
+    {
+    }
+
+    /**
+     * @param mixed $from
+     * @param mixed $to
+     */
+    public function update($from, $to)
+    {
+    }
+}
